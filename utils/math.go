3c3d4a212d479e1e79886323c1a4e7c5d09100de
M Algah Fattah Illahi <malgahfattahillahi@gmail.com>
2023-09-28 16:40:24 +0700

    fix: randomize seed

--- utils/math.go
+++ utils/math.go
@@ -1,8 +1,12 @@
 package utils
 
-import "math/rand"
+import (
+	"math/rand"
+	"time"
+)
 
 // GenerateRandomInt generates a random integer between min and max: [min, max)
 func GenerateRandomInt(min, max int) int {
+	rand.Seed(time.Now().UnixNano())
 	return min + rand.Intn(max-min)
 }
